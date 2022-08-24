b6b6df0538a729996239212ce7cd0af59516d0d8
Sararchh <sara_rocha99@hotmail.com>
2022-08-24 17:56:18 -0300

    feat:add logar

--- home/script.js
+++ home/script.js
@@ -1,3 +1,5 @@
+let messages = [];
+
 function showModal() {
   modal = document.querySelector('.navbar');
 
@@ -23,3 +25,35 @@ function checkItemParticipant(elementIcon) {
   }
   elementIcon.childNodes[3].classList.add('showIconCheck');
 }
+
+//buscar messagens
+function searchMessage() {
+  const promisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+  promisse.then(getMessages)
+}
+
+searchMessage();
+
+function getMessages(response) {
+  messages = response.data;
+
+  renderMessages();
+}
+
+
+function renderMessages(){
+  const ul = document.querySelector('.chatMessages');
+
+  ul.innerHTML = '';
+
+  for(let i = 0; i < messages.length; i++){
+
+    ul.innerHTML = ul.innerHTML + `
+        <li class="messageChat">
+          <p> <span>${messages[i].time}</span> <strong>${messages[i].from}</strong> para <strong>${messages[i].to} </strong> ${messages[i].text}</p>
+        </li>
+    `;
+}
+}
+
+renderMessages()
